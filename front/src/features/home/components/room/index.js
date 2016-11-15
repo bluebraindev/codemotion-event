9124c11f4c5721007a0eeb17f687ecd8d560c47d
OscarJunior <oscar.gonzales@edu.uah.es>
2016-11-15 16:36:31 +0100

    [CODEMOTION][FRONT][FIX] Eliminando carpeta components

--- front/src/features/home/components/room/index.js
+++
@@ -1,9 +0,0 @@
-import RoomController from './controller';
-
-export default {
-    bindings: {
-        name: '='
-    },
-    templateUrl: 'src/features/home/components/room/template.html',
-    controller: RoomController
-}; 
