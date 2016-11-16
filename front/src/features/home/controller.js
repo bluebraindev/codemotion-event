f41fe1a2c9602581011e53be9011c10d8a2bdad8
OscarJunior <oscar.gonzales@edu.uah.es>
2016-11-16 17:11:02 +0100

    [CODEMOTION][FRONT][FIX] Maquetación de Home, componente room terminado.

--- front/src/features/home/controller.js
+++ front/src/features/home/controller.js
@@ -1,23 +1,63 @@
 export default function HomeController() {
     'ngInject'
     var vm = this;
-    vm.rooms = [
-        {
-            name: 'A'
-        },
-        {
-            name: 'B'
-        },
-        {
-            name: 'C'
-        },
-        {
-            name: 'D'
-        },
-        {
-            name: 'E'
-        }
-    ];
 
-    
+    init();
+
+    vm.isDefinedRoom = isDefinedRoom;
+
+    vm.getNameFromRoom = getNameFromRoom;
+
+    function getNameFromRoom(position){
+        return vm.rooms[position].name;
+    }
+
+    function isDefinedRoom(position) { 
+        return vm.rooms[position];
+    }
+
+    function init() {
+        vm.header = 'Tracks';
+        vm.rooms = [
+            {
+                name: '1'
+            },
+            {
+                name: '2'
+            },
+            {
+                name: '3'
+            },
+            {
+                name: '4'
+            },
+            {
+                name: '5'
+            },
+            {
+                name: '6'
+            },
+            {
+                name: '7'
+            },
+            {
+                name: '8'
+            },
+            {
+                name: '9'
+            },
+            {
+                name: 'A'
+            },
+            {
+                name: 'B'
+            },
+            {
+                name: 'C'
+            },
+            {
+                name: 'D'
+            }
+        ];
+    }
 }
