141f1bd6854c41420ba9c1f22d45ff0a2666001b
OscarJuniorGonzalesFlores <oscar.gonzales@edu.uah.es>
2016-11-16 22:14:36 +0100

    [COFFEE][FEAT][FRONT] Integrando servicio de salas en Home.

--- front/src/features/home/controller.js
+++ front/src/features/home/controller.js
@@ -1,64 +1,27 @@
-export default function HomeController() {
+export default function HomeController($agenda) {
     'ngInject'
-    
+
     var vm = this;
 
     init();
-
-    vm.isDefinedRoom = isDefinedRoom;
-
+    
     vm.getNameFromRoom = getNameFromRoom;
 
-    function getNameFromRoom(position){
+    vm.rooms = [];
+
+    function getNameFromRoom(position) {
         return vm.rooms[position].name;
     }
 
-    function isDefinedRoom(position) { 
-        return vm.rooms[position];
-    }
-
     function init() {
-        vm.header = 'Tracks';
-        vm.rooms = [
-            {
-                name: '1'
-            },
-            {
-                name: '2'
-            },
-            {
-                name: '3'
-            },
-            {
-                name: '4'
-            },
-            {
-                name: '5'
-            },
-            {
-                name: '6'
-            },
-            {
-                name: '7'
-            },
-            {
-                name: '8'
-            },
-            {
-                name: '9'
-            },
-            {
-                name: 'A'
-            },
-            {
-                name: 'B'
-            },
-            {
-                name: 'C'
-            },
-            {
-                name: 'D'
-            }
-        ];
+        vm.header = 'Codemotion';
+        $agenda.getTracks()
+            .then((res) => {
+                res.data.forEach(justTracksToOrdinalDay)
+            })
+
+        function justTracksToOrdinalDay(track, index) {
+            index < 12 ? vm.rooms.push(track) : undefined;
+        }
     }
 }
