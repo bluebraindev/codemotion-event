fcce2ea73ef9299797dcd815b0c342a23691e598
Alberto Seoane <aseoanecule@hotmail.com>
2016-11-17 16:34:01 +0100

    [FEAT] [FRONT] [EVENT-DETAILS] Implementada la llamada al servicio que devuelve el EVENTO buscado por ID con los datos del TALKS.json y creado Template a falta de maquetar

--- front/src/features/home/event-details/controller.js
+++ front/src/features/home/event-details/controller.js
@@ -3,6 +3,8 @@ export default function EventDetailsController($scope, $state, $eventDetails, $a
 
     var vm = this;
     var talks = [];
+    vm.authors = [];
+    vm.tagsEvent = {};    
 
     init();
 
@@ -10,8 +12,11 @@ export default function EventDetailsController($scope, $state, $eventDetails, $a
         vm.header = 'Event Details';
 
         $agenda.getTalks().then(() => {
-            var talk = $agenda.getTalkById(83004001);
+            var talk = $agenda.getTalkById(85544001);
             vm.eventDetail = talk[0];
-        })
+            vm.authors = talk[0].contents.authors;
+            vm.tagsEvent = talk[0].contents.tags;
+                    
+        });
     }
 }
